eada7888763413c6b2bc457f6c27c79871fdf5cc
fvtre <ad.lecaros@duocuc.cl>
2026-08-18 12:13:53 -0400

    speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from 'next'
 import { Inter, Geist_Mono } from 'next/font/google'
 import Script from 'next/script'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Toaster } from '@/components/ui/sonner'
 import './globals.css'
 
